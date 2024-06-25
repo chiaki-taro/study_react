9da2e672e4cf678b5b287e4f9907b7ebf1f1dc3d
chiaki-taro <chias.926@gmail.com>
2024-06-25 13:07:55 +0900

    変数と同名のプロパティ

--- my-modern/obj_prop.js
+++ my-modern/obj_prop.js
@@ -2,6 +2,6 @@ const title = '速習React';
 const price = 500;
 
 const book = { title, price };
-// const book = { title: title, price: price };
+// const book = { title: title, price: price }; 変数と同名のプロパティは省略可能
 
 console.log(book);
