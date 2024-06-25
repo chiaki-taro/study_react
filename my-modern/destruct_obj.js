8b34fd8828d479001d7c84957fa02c5521f34675
chiaki-taro <chias.926@gmail.com>
2024-06-25 13:51:57 +0900

    オブジェクトの分割代入

--- my-modern/destruct_obj.js
+++ my-modern/destruct_obj.js
@@ -3,7 +3,11 @@ const member = {
   sex: 'female',
   age: 18
 };
+//オブジェクトの場合は、名前で割り当て先を決めるので、並びはプロパティの定義順と異なっても問題はない。
+//割り当てのないプロパティがあっても構わない。（この場合ageプロパティが無視される）
+//目的のプロパティが存在しなかった場合に備えて「変数名＝規定値」の形式で規定値を設定することも可能。
 const { fullname, sex, memo = '---' } = member;
+
 
 // let fullname, sex, memo;
 // ({ fullname, sex, memo = '---' } = member);
