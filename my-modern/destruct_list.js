c3839ff529ca18933ae5a7b573de7fca5b056e3b
chiaki-taro <chias.926@gmail.com>
2024-06-25 13:27:39 +0900

    分割代入

--- my-modern/destruct_list.js
+++ my-modern/destruct_list.js
@@ -3,10 +3,10 @@ const [x, y, z] = list;
 console.log(x, y, z);
 
 const [a, b] = list;
-console.log(a, b);
+console.log(a, b); //個数が少なければ残りの要素は無視される
 
 const [l, m, n, o] = list;
-console.log(l, m, n, o);
+console.log(l, m, n, o); //個数が多い場合はあふれた変数は「undefined」となる
 
-const [p, , r] = list;
+const [p, , r] = list; //一部の変数と読み飛ばす
 console.log(p, r);
--- my-modern/destruct_list_rest.js
+++ my-modern/destruct_list_rest.js
@@ -1,3 +1,3 @@
 const list = [10, 20, 30];
 const [one, ...rest] = list;
-console.log(one, rest);
+console.log(one, rest); //分割しきれなかった残りの要素を「...」演算子をつけた変数restに格納
