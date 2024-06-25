0c7662e1e6e0262f91bb3367449deab4d966cc34
chiaki-taro <chias.926@gmail.com>
2024-06-25 10:41:24 +0900

    constの性質

--- my-modern/const.js
+++ my-modern/const.js
@@ -1,3 +1,8 @@
 const author = 'YAMADA.Yoshihiro';
-author = 'WINGS Project';
+// author = 'WINGS Project'; constは定数なので再代入できない。
 console.log(author);
+
+const list = ['React', 'Vue', 'Augular'];
+list[0] = 'Roit'; //constは「再代入できない」であって、「変更できない」ではない。中身の要素を入れ替えることは可。
+// list = ['Java', 'C#', 'Python']; listに対して再代入を試みているためNG
+console.log(list); // [ 'Roit', 'Vue', 'Augular' ]
