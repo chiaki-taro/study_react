05840bc8c2802defdde21f2385e5e82dd46c4485
chiaki-taro <chias.926@gmail.com>
2024-06-25 16:05:16 +0900

    引数の初期値構文

--- my-modern/func_def.js
+++ my-modern/func_def.js
@@ -1,6 +1,9 @@
 function getTrapezoidArea(upper = 1, lower = 1, height = 1) {
   return (upper + lower) * height / 2;
 }
+//仮引数名=初期値で引数に初期値を設定できる。
+//初期値に式を渡すこともできる。
+//function getTrapezoidArea(upper = 1, lower = upper, height = upper) {...};
 
 console.log(getTrapezoidArea(10, 5, 3));
 console.log(getTrapezoidArea(10, 5))
